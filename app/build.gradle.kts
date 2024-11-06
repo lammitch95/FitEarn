5e1ae4dc99618beb2dd6b9263c68ae0b81816d33
dmondal06 <deepa.mondal321@gmail.com>
2024-11-05 19:33:10 -0500

    project structure updated. kotlin 1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.kotlin.android)
+    // id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -51,49 +52,27 @@ android {
 }
 
 dependencies {
-
-    // Define App Center SDK Version
-    val appCenterSdkVersion = "5.0.4"
-
-    // App Center Dependencies
-    implementation(libs.appcenter.analytics)
-    implementation(libs.appcenter.crashes)
-
-    // Firebase BoM for consistent versions of Firebase libraries
+   // implementation(libs.kotlinx.coroutines.core)
+   // implementation(libs.androidx.lifecycle.runtime.ktx.v240)
     implementation(platform(libs.firebase.bom))
-
-    // Firebase Authentication
-    implementation(libs.google.firebase.auth)
-
-    // Google Play services authentication (for Google Sign-In)
-    implementation(libs.play.services.auth)
-
-    // Firebase Analytics (Optional)
-    implementation(libs.google.firebase.analytics)
-
-    // AndroidX libraries
+    implementation(libs.firebase.analytics)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-
-    // Compose dependencies
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
-
-    // Testing dependencies
+  //  implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-
-    // Debug dependencies
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    //implementation(libs.androidx.datastore.preferences)
 }
