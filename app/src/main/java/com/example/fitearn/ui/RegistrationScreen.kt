2c0c2ce66141dd5d6d75668e844f6af15d259473
Mitchell Lam <lammitch95@gmail.com>
2024-11-05 23:56:53 -0500

    just outline checkbox wasnt visible before

--- app/src/main/java/com/example/fitearn/ui/RegistrationScreen.kt
+++ app/src/main/java/com/example/fitearn/ui/RegistrationScreen.kt
@@ -1,6 +1,7 @@
 package com.example.fitearn.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -187,9 +188,10 @@ fun RegistrationPage(navController: NavController) {
                 onCheckedChange = { termCondition = it },
                 colors = CheckboxDefaults.colors(
                     checkedColor = Color.White,
-                    uncheckedColor = Color.Transparent,
+                    uncheckedColor = Color.White,
                     checkmarkColor = Color.White
-                )
+                ),
+
             )
             Text(
                 text = "Agree with Terms & Conditions",
